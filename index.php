3354f44cf3d868a81597455a33e089ce2940784f
viktorijagardauskiene <viktorija.gardauskiene@gmail.com>
2017-06-23 20:29:57 +0300

    200 black cells

---
+++ index.php
@@ -0,0 +1,44 @@
+<!DOCTYPE html>
+<html>
+<body>
+<meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+<style>
+		td {
+			padding: 10px;
+			border-color: white;
+			border: solid;
+			background-color: black;
+			}
+
+		table {
+			border-color: black;
+			border: solid;
+			}
+
+</style>
+<div class="container">
+	<div class="langeliai">
+	<?php
+		$end=200; 
+		$rows=ceil($end/25);
+		$x=0;
+		$start=1;
+		?>
+		<table>
+		<?php
+		while($x<=$rows) {
+		  echo "<tr>";
+		  for ($y = 0; $y < 25; $y++, $start++) {
+		    if ($start <= $end) echo "<td>$start</td>";
+		  }
+		  echo "</tr>";
+		  $x++;
+		}
+		?>
+</table>
+	</div>
+</div>
+</body>
+</html>
