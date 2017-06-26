e9c6819fe2a7f9725ee50999c2002ea94a0eea87
viktorijagardauskiene <viktorija.gardauskiene@gmail.com>
2017-06-26 19:09:47 +0300

    funkcijos su tekstu, du kintamieji tekste
    3 kart 1 yra 3 // kai daugiklis yra nuo 1 iki 10

--- index.php
+++ index.php
@@ -1,58 +1,22 @@
-<?php
-		require "function.php";
-		//include "function.php";
-	   
-		?>
-
 <!DOCTYPE html>
 <html>
 <head>
-	<meta charset="utf-8">
- 	<meta name="viewport" content="width=device-width, initial-scale=1">
- 	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
-   	<style>
-		.langelis {
-			width: 40px;
-			padding: 10px;
-			background-color: gray;
-			display: block;
-			float: left;
-			margin: 1px;
-			}
-		.eilute {
-			border-color: black !important;
-			
-		}
-	</style>
+	<title></title>
 </head>
 <body>
-<div class="container">
-			
-	<div class="oro_temperatura">
-			
-	<div class="row">
-		<div class="col-md-2"></div>
-		<div class="col-md-8">
-			<table class="table table-bordered">
-				<th class='eilute'>Baseino gylis</th>
-				<th class='eilute'>Tūris</th>
-				<th class='eilute'>Autocisternų kiekis</th>
-					<?php
-					for ($gylis=1; $gylis <= 30 ; $gylis++) { 			
-					
-						$auto_sk=ceil(baseino_turis($ilgis,$plotis,$gylis)/$auto_turis);
-				
-						echo "<tr><td class='eilute'>".$gylis." m."."</td><td class='eilute'>".baseino_turis($ilgis,$plotis,$gylis)." kv. m."."</td><td class='eilute'>". $auto_sk." vnt."."</td></tr>";
-						}
-					?>
-			</table>
-		</div>
-			
-		<div class="col-md-2"></div>
-	</div>
 
-	</div>
+<?php
+ 
+// parašyti "3 kart 1 yra 3" kai daugiklis yra nuo 1 iki 10
+for ($i=1; $i <= 10 ; $i++) { 
+	$rezultatas = 3*$i;
 
-</div>
+	echo sprintf("3 kart %s yra %s <br />", $i , $rezultatas); // string print format (sprintf) %s yra kintamasis, kuris nurodytas toliau skliausteliuose, o funkcija įterps juos rodydama.
+}
+
+
+?>
+
+
 </body>
 </html>
---
+++ pamokatekstas.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title></title>
+</head>
+<body>
+
+<?php
+ 
+
+for ($i=1; $i <= 10 ; $i++) { 
+	$rezultatas = 3*$i;
+
+	echo sprintf("3 kart %s yra %s", $i , $rezultatas);
+}
+
+
+?>
+
+
+</body>
+</html>
