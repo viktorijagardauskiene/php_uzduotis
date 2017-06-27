860afe5cf3c80348a7a887a90733e64e78eda3f2
viktorijagardauskiene <viktorija.gardauskiene@gmail.com>
2017-06-27 22:06:57 +0300

    jautienos troskinio produktai lenteleje

--- index.php
+++ index.php
@@ -2,77 +2,12 @@
 <html>
 <head>
 	<title></title>
+	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
 </head>
 <body>
 
 <?php
  
-// parašyti "3 kart 1 yra 3" kai daugiklis yra nuo 1 iki 10
-for ($i=1; $i <= 10 ; $i++) { 
-	$rezultatas = 3*$i;
-
-	echo sprintf("3 kart %u yra %u <br />", $i , $rezultatas); // string print format (sprintf) %s (su procento zenklu) yra kintamasis, kuris nurodytas toliau skliausteliuose, o funkcija įterps juos rodydama.
-}
-
-?>
-
-<pre>
-<?php
-
-$skaiciai = [22, 33, 44, 55, 66];
-
-for ($i=0; $i < count($skaiciai) ; $i++) { // masyvo $skaiciai kiekviena elementa padaugina is dvieju
-	$skaiciai[$i] = $skaiciai[$i]*2;
-}
-print_r($skaiciai); // atspausdina masyvio elementus
-
-function spausdinti($masyvas) { // atskira funkcija atspausdina masyvo elementus
-	foreach ($masyvas as $elementas) { 
-	echo $elementas . "<br />";
-}
-}
-
-spausdinti($skaiciai);
-
-echo "<br />";
-
-// uzduotis sukurti funkcija kuri atspausdintu masyvo elementus kaip sarasa <ul>
-
-$gerimai = ["Vanduo", "Sultys", "Arbata", "Kava", "Pienas"];
-
-
-// uzduotis sukurti funkcija kuri atspausdins random gerima kiekviena karta vis kita
-
-function random_gerimas($gerimai) {
-
-$gerimas = strtolower($gerimai[rand(0, count($gerimai)-1)]);
-
-
-echo sprintf("Mano mėgstamiausias gėrimas yra %s", $gerimas) . "."; 
-}
-
-random_gerimas($gerimai);
-
-echo "<br />";
-
-$users = [];
-$user1 = ["name" => "Petras", "surname" => "Jonaitis",];
-$user2 = ["name" => "Mantas", "surname" => "Eimaitis",];
-
-array_push($users, $user1, $user2); // masyve $users yra kiti du masyvai (su savo elementais) Masyve[masyvas];
-
-print_r($users);
-
-echo "<br />";
-
-echo $users[0]["name"]; // nurodo masyve esancio pirmojo masyvo "name" elementa
-
-echo "<br />";
-
-foreach ($users as $kitas) { // grazina kiekvieno masyvo name ir surname elementu reiksmes
-	echo $kitas["name"]. " " . $kitas["surname"]."<br />";
-}
-
 // uzduotis namu darbams 
 /* Suvesti i masyva troskinio sudeti:
 jautiena 250 g
@@ -86,10 +21,49 @@ paprika 100 g
 3. parasyti funkcija kuri suskaiciuotu kiek svers visi produktai
 4. parasyti funkcija kuri patikrintu ar vandens ne maziau nei pupeliu */
 
+$produktai = [
+		"jautiena" => 250, 
+		"pupeles" => 500, 
+		"vanduo" => 500, 
+		"pomidorai" => 300, 
+		"paprika" => 100,
+		];
+
+function lentele($produktai) {
+	foreach ($produktai as $prod => $kiekis) {
+    echo '<tr>';
+    echo "<td>" . $prod . "</td><td>" . $kiekis . "</td>";
+    echo '</tr>';
+  }
+}
+
+function receptas($produktai) {
+foreach ($produktai as $prod => $kiekis) {
+print_r($kiekis);
+
+
+//echo sprintf("Jautienos troškiniui pagaminti mums reikės %i gramų %s, taip pat %i gramų %s, be to %i mililitrų %s, %i gramų %s ir %i gramų %s",  ;
+
+}
+}
+?>
+
+<div class="container">
+
+<table class="table-bordered">
+<tr>
+	<?php lentele($produktai); ?>
+</tr>
+</table>
+
+
+<?php 
+
+	receptas($produktai); 
 
 ?>
-</pre>
+
+</div>
 
 </body>
 </html>
-
