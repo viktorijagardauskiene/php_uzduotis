4746d4187932b1e1648a7554372bbd167d2bda7d
viktorijagardauskiene <viktorija.gardauskiene@gmail.com>
2017-06-28 19:27:59 +0300

    black-white langeliai 3x3

--- image.php
+++ image.php
@@ -1,42 +1,36 @@
 <style>
-	.y {
-		background-color: yellow;
-		height: 30px;
-		width: 30px;
+	.j {
+		background-color: black;
+		height: 50px;
+		width: 50px;
 		display: block;
 		float: left;
 		}
 
-	.g {
-		background-color: green;
-		height: 30px;
-		width: 30px;
+	.b {
+		background-color: white;
+		height: 50px;
+		width: 50px;
 		display: block;
 		float: left;
 		}
 
-	.r {
-		background-color: red;
-		height: 30px;
-		width: 30px;
-		display: block;
-		float: left;
-		}
+	
 </style>
 
 
 
 <?php
 
-$img = ["y", "y", "y", "y", "g", "g", "g", "g", "r", "r" , "r", "r"];
+$img = ["j", "b", "j", "b", "j", "b", "j", "b", "j"];
 
 $hp = 0;
 
 foreach ($img as $element) {
-	echo '<div class="'. $element .'">' . $element . '</div>'; // $element yra kaip klases pavadinimas, todel tiek kabuciu
+	echo '<div class="'. $element .'"></div>'; // $element yra kaip klases pavadinimas, todel tiek kabuciu
 	$hp++;
-		if($hp == 4) {
-			echo "<br /><br />";
+		if($hp == 3) {
+			echo "<br /><br /><br />";
 			$hp = 0;
 		}
 
