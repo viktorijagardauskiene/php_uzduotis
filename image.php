c6f4a9cda22a22295e3cae25fb9fb2bd30d9d391
viktorijagardauskiene <viktorija.gardauskiene@gmail.com>
2017-06-28 19:45:23 +0300

    skaiciuoja kiek reikes juodu korteliu

--- image.php
+++ image.php
@@ -19,7 +19,6 @@
 </style>
 
 
-
 <?php
 
 $img = ["j", "b", "j", "b", "j", "b", "j", "b", "j"];
@@ -28,12 +27,20 @@ $hp = 0;
 
 foreach ($img as $element) {
 	echo '<div class="'. $element .'"></div>'; // $element yra kaip klases pavadinimas, todel tiek kabuciu
-	$hp++;
-		if($hp == 3) {
+	$hp++; // hp skaiciuoja kiek ciklu
+		if($hp == 3) { // po trecio ciklo perkelia i kita eilute
 			echo "<br /><br /><br />";
-			$hp = 0;
+			$hp = 0; // grazina hp reiksme i 0
 		}
 
 }
 
+$juodos_korteles = 0;
+foreach ($img as $kortele) {
+	if ($kortele == "j") {
+		++$juodos_korteles;
+			}
+}
+echo "<br />";
+echo "Mums reikes ".$juodos_korteles." juodu korteliu.";
 ?>
